efbb82a2cef4533fcb28d7ee96ecac76ed4ac6b7
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-07-23 12:49:07 +0000

    Create multiple tables

--- data.sql
+++ data.sql
@@ -11,3 +11,15 @@ VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
 ('Boarmon', '2005-06-07', 7, true, 20.4), 
 ('Blossom', '1998-10-13', 3, true, 17), 
 ('Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+       ('Jennifer Orwell', 19),
+       ('Bob', 45),
+       ('Melody Pond', 77),
+       ('Dean Winchester', 14),
+       ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon'),
+       ('Digimon');
